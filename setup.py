6d80d1e6a532a3d228105e67b566fb67e91ca778
Kenneth Reitz <me@kennethreitz.com>
2012-10-27 10:44:37 -0500

    v0.2.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ required = [
 
 setup(
     name='django-postgrespool',
-    version='0.2.2',
+    version='0.2.3',
     description='Postgres Connection Pooling for Django.',
     long_description=open('README.rst').read(),
     author='Kenneth Reitz',
