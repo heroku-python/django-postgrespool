88022f06bc0e26e72c1bfe0b2a72f90e523cb5cd
Jonty Wareing <jonty@jonty.co.uk>
2013-07-03 11:51:10 +0000

    Time for a new release!
    There's been multiple fairly critical fixes applied, so bumping the
    version number for a new release.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ required = [
 
 setup(
     name='django-postgrespool',
-    version='0.2.3',
+    version='0.2.4',
     description='Postgres Connection Pooling for Django.',
     long_description=open('README.rst').read(),
     author='Kenneth Reitz',
